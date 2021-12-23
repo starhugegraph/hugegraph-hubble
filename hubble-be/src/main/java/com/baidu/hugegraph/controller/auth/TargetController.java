b6b1ebf0a0ed323e5c6e6977c03f0f5ad4001eb2
logic4nil <logic4nil@gmail.com>
2021-12-23 17:34:47 +0800

    feature: target

--- hubble-be/src/main/java/com/baidu/hugegraph/controller/auth/TargetController.java
+++ hubble-be/src/main/java/com/baidu/hugegraph/controller/auth/TargetController.java
@@ -2,7 +2,7 @@ package com.baidu.hugegraph.controller.auth;
 
 import java.util.List;
 
-import com.baidu.hugegraph.driver.HugeClient;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baidu.hugegraph.common.Constant;
 import com.baidu.hugegraph.service.auth.TargetService;
 import com.baidu.hugegraph.structure.auth.Target;
+import com.baidu.hugegraph.driver.HugeClient;
 
 @RestController
 @RequestMapping(Constant.API_VERSION + "graphspaces/{graphspace}/auth/targets")
@@ -23,11 +25,23 @@ public class TargetController extends AuthController {
     @Autowired
     TargetService targetService;
 
-    @GetMapping
     public List<Target> list(@PathVariable("graphspace") String graphSpace) {
 
         HugeClient client = this.authClient(graphSpace, null);
         return this.targetService.list(client);
+    }
+
+    @GetMapping
+    public IPage<Target> queryPage(@PathVariable("graphspace") String graphSpace,
+                                   @RequestParam(name = "query", required = false,
+                                           defaultValue = "") String query,
+                                   @RequestParam(name = "page_no", required = false,
+                                           defaultValue = "1") int pageNo,
+                                   @RequestParam(name = "page_size", required = false,
+                                           defaultValue = "10") int pageSize) {
+
+        HugeClient client = this.authClient(graphSpace, null);
+        return this.targetService.queryPage(client, query, pageNo, pageSize);
     }
 
     @GetMapping("{id}")
@@ -38,10 +52,13 @@ public class TargetController extends AuthController {
     }
 
     @PostMapping
-    public void add(@PathVariable("graphspace") String graphSpace,
+    public Target add(@PathVariable("graphspace") String graphSpace,
                     @RequestBody Target target) {
         HugeClient client = this.authClient(graphSpace, null);
-        this.targetService.add(client, target);
+        // The field `url` is not currently used
+        // The server requires that the field `url` cannot be null
+        target.url("");
+        return this.targetService.add(client, target);
     }
 
     @PutMapping("{id}")
@@ -51,9 +68,7 @@ public class TargetController extends AuthController {
         HugeClient client = this.authClient(graphSpace, null);
         Target t = this.targetService.get(client, tid);
         t.resources(target.resources());
-        this.targetService.update(client, t);
-
-        return t;
+        return this.targetService.update(client, t);
     }
 
     @DeleteMapping("{id}")
--- hubble-be/src/main/java/com/baidu/hugegraph/service/auth/TargetService.java
+++ hubble-be/src/main/java/com/baidu/hugegraph/service/auth/TargetService.java
@@ -1,8 +1,12 @@
 package com.baidu.hugegraph.service.auth;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.baidu.hugegraph.driver.HugeClient;
+import com.baidu.hugegraph.util.PageUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.springframework.stereotype.Service;
 import lombok.extern.log4j.Log4j2;
 
@@ -20,6 +24,18 @@ public class TargetService extends AuthService{
         return targets;
     }
 
+    public IPage<Target> queryPage(HugeClient client, String query, int pageNo,
+                                   int pageSize) {
+
+        List<Target> results =
+                list(client).stream()
+                            .filter(target -> target.graph().contains(query))
+                            .sorted(Comparator.comparing(Target::name))
+                            .collect(Collectors.toList());
+
+        return PageUtil.page(results, pageNo, pageSize);
+    }
+
     public Target get(HugeClient client, String tid) {
         Target target = client.auth().getTarget(tid);
         if (target == null) {
@@ -28,12 +44,12 @@ public class TargetService extends AuthService{
         return target;
     }
 
-    public void add(HugeClient client, Target target) {
-        client.auth().createTarget(target);
+    public Target add(HugeClient client, Target target) {
+        return client.auth().createTarget(target);
     }
 
-    public void update(HugeClient client, Target target) {
-        client.auth().updateTarget(target);
+    public Target update(HugeClient client, Target target) {
+        return client.auth().updateTarget(target);
     }
 
     public void delete(HugeClient client, String tid) {
