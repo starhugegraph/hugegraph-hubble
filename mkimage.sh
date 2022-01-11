b959ec8e0a650031dc34110b1587c22f385721a8
guoygang <hero.gariker@gmail.com>
2022-01-11 18:34:43 +0800

    add docker script

--- mkimage.sh
+++ mkimage.sh
@@ -7,7 +7,7 @@ docker pull ubuntu:16.04
 docker build -t hugegraph-hubble:3.0.0 -f Dockerfile .
 
 # test
-# docker run -d --name hubble -p 127.0.0.1:8088:8088 hugegraph-hubble:3.0.0
+# docker run -itd -p 8089:8088 --name hubble hugegraph-hubble:3.0.0
 
 # save image
 # docker save hugegraph-hubble:3.0.0 > hugegraph-hubble-3.0.0.tar
